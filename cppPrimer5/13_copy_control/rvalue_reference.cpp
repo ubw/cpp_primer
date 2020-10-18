1daf9a714ac93c34bcecf44ca652bea91d169b95
jwchu <cjwstorm@163.com>
2020-10-18 15:18:28 +0800

    20201018

--- cppPrimer5/13_copy_control/rvalue_reference.cpp
+++ cppPrimer5/13_copy_control/rvalue_reference.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+//4. 引用折叠
+template<typename T>
+void f(T&& t){}
+
+void g(int&& t){}
+
 int main(void)
 {
     //1. c++11 右值引用
@@ -16,5 +22,12 @@ int main(void)
     //3. 右值引用也是一个变量,不能绑定到右值引用上,和2不是一个意思么
     // cannot bind ‘int’ lvalue to ‘int&&’
     int &&rr3 = rr;
+
+    f(10);
+    int x = 10;
+    f(x);
+
+    //5. cannot bind ‘int’ lvalue to ‘int&&’
+    g(x);
 
 }
